aad3910be2f0379d27f1372cce2aacc4347f8a74
kangfei <kang_chao520@163.com>
2016-12-22 00:18:45 +0800

    f

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -211,7 +211,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10,tolerance=0.005)
+    sim.run(n_test=20,tolerance=0.005)
     #sim.run(n_test=10)
 
 
